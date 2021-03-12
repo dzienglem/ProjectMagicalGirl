0368dc8c3c2e2df171e21e41af03b6e18adfdfc6
Jem <dinsjemx@gmail.com>
2021-03-12 23:36:40 +0900

    格式调整

--- md_edit.py
+++ md_edit.py
@@ -2,7 +2,7 @@ import glob
 import os
 
 print (os.getcwd())
-num = 1
+num = 8
 f_name = "{}.md".format(num)
 f_path = os.path.join(os.getcwd(),f_name)
 print (f_path)
@@ -11,11 +11,13 @@ file_data = ""
 
 with open (f_path, "r", encoding='utf-8') as f:
     for line in f:
-        if ("\\" not in line) and ("***" not in line):
-            line = line[:-1]+"\\"+line[-1]
+        if ("  " not in line) and ("***" not in line):
+            line = line[:-1]+"  "+line[-1]
         file_data = file_data + line
 
-file_data = file_data + "[Next]({}.md) ".format(num+1)
+if file_data[-2] != ")":
+    print (file_data[-2])
+    file_data = file_data + "[Next]({}.md) ".format(num+1)
 
 with open(f_path,"w",encoding="utf-8") as f:
         f.write(file_data)
